8309a1ecb4d1690efb420765d1a2b54fd07faaf8
andreainno22 <andrea.innocenti12@stud.unifi.it>
2023-06-12 10:41:43 +0200

    2.6

--- PPM_app/settings.py
+++ PPM_app/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
-import DATABASES as DATABASES
 from django.conf import settings
 import dj_database_url
 
@@ -35,9 +33,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-kcvy6(9e8moyx0
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["djangorailway-production.up.railway.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["djangorailway-production-a2927.up.railway.app", "127.0.0.1", "localhost"]
 
-CSRF_TRUSTED_ORIGINS = ['https://djangorailway-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://djangorailway-production-a2927.up.railway.app']
 
 # Application definition
 
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn PPM_app.wsgi --log-file -
+
