bc6568773cfbadac685937b94855c9927ad88149
andreainno22 <andrea.innocenti12@stud.unifi.it>
2023-06-12 11:53:21 +0200

    3.1

--- PPM_app/settings.py
+++ PPM_app/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+
+import DATABASES as DATABASES
 from django.conf import settings
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
